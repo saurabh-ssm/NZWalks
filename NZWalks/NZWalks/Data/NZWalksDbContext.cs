f26adc9591c3017b88a30899d15cb8b43a467678
xeno <Saurabhmehetresm@gmail.com>
2022-12-03 19:12:17 +0530

    End Of section two

---
+++ NZWalks/NZWalks/Data/NZWalksDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.Models.Domain;
+
+namespace NZWalks.Data
+{
+    public class NZWalksDbContext:DbContext
+    {
+        public NZWalksDbContext(DbContextOptions<NZWalksDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+
+        public DbSet<WalkDifficulty> WalkDifficultys { get; set; }
+    }
+}
---
+++ NZWalks/NZWalks/Models/Domain/Region.cs
@@ -0,0 +1,18 @@
+﻿namespace NZWalks.Models.Domain
+{
+    public class Region
+    {
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public double Area { get; set; }
+        public double Lat { get; set; }
+        public double Lon { get; set; }
+        public long Population { get; set; }
+
+
+        // navigation property
+
+        public IEnumerable<Walk> walks { get; set; }
+    }
+}
